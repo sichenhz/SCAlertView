6083e0c5ec89af252407f71cd09f5a2fbd1d4303
sichenwang <52870104@qq.com>
2016-03-08 22:34:15 +0800

    Initial Commit

---
+++ SCAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCAlertView
+//
+//  Created by sichenwang on 16/3/8.
+//  Copyright © 2016年 sichenwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCAlertView
+//
+//  Created by sichenwang on 16/3/8.
+//  Copyright © 2016年 sichenwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
